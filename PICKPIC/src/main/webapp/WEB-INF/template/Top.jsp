8b913ca3f827d32f96058cb9ca26d3bc8907566c
SeulKi <qkrpsg@gmail.com>
2019-04-22 20:03:29 +0900

    Top.jsp Top.css Top.js 수정중... 플러그인 다운로드를 위해 임시 저장

--- PICKPIC/src/main/webapp/WEB-INF/template/Top.jsp
+++ PICKPIC/src/main/webapp/WEB-INF/template/Top.jsp
@@ -1,141 +1,150 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib  prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<link href="<c:url value='/Bootstrap/css/bootstrap.min.css'/>" rel="stylesheet">
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<link href="<c:url value='/Bootstrap/css/bootstrap.min.css'/>"
+	rel="stylesheet">
 <link href="<c:url value='/css/Top.css'/>" rel="stylesheet">
-<script src="<c:url value='/Bootstrap/js/bootstrap.min.js'/>"></script>   
-<script src="<c:url value='/js/mostrar_nav.js'/>"></script>   
+<script src="<c:url value='/Bootstrap/js/bootstrap.min.js'/>"></script>
+<script src="<c:url value='/js/mostrar_nav.js'/>"></script>
 <script src="<c:url value='/js/Top.js'/>"></script>
 <!--  -->
-		<header class="container-fluid header_top ">
- 
-				<div class="menu_all_wrap_position">
-				
-					<div class="logo"><a href="<c:url value='/PICKPIC/admin/home.pic'/>"><img src="<c:url value='/resources/images/a1.PNG'/>" /></a></div>
-					
-						<div class="menu_login_wrap">
-							<div class="menu_login_in">
-								<nav class="menu_wrap">
-									<ul>
-										<li><a href="#">피크픽 스토리</a></li>
-										<li><a href="#">피크픽 프렌즈</a></li>
-										<li><a href="#">고객센터</a></li>
-									</ul>
-									
-								</nav>
-							 
-								
-								<div class="login_wrap">
-									<ul>
-										<li><a href="<c:url value='/PickPic/login/Login.bbs'/>">LOGIN<span></span></a>
-										<li><a href="#">SING UP</a>
-									</ul>
-								</div>
-						 </div>
-					 
-					 </div>
-					 
-					 <!-- 모바일 용 메뉴 -->
-					 <div id="mobile-nav">
-							 <nav>
-								 <ul class="menu" >
-								 
-								 
-									 <li class="l_menu_li"><a href="#">피크픽 스토리</a>
-											 <ul class="m_sub" >
-													 <li><a href="<c:url value='/PICKPIC/story/back.pic'/> "> 개발배경</a></li>
-													 <li><a href="<c:url value='/PICKPIC/story/culture.pic'/> ">피크픽 문화</a></li>
-													 <li><a href="#">팀원소개</a></li>
-													 <li><a href="#">앞으로의 행보</a></li>
-											 </ul>
-									 </li>
-									 
-									 
-									 <li class="l_menu_li"><a href="#">피크픽 프렌즈</a>
-										 <ul class="m_sub" style="display: none;">
-													 <li><a href="#">필터링</a></li>
-													 <li><a href="<c:url value='/PickPic/friends/Place.bbs'/>">명소찾아주기</a></li>
-													 <li><a href="#">피크픽추천</a></li>
-											 </ul>
-									 </li>
-									 
-									 
-									 <li class="l_menu_li"><a href="#">고객센터</a>
-										 <ul class="m_sub" style="display: none;">
-													 <li><a href="#">피크픽 이렇게 활용하세요</a></li>
-													 <li><a href="<c:url value='/PICKPIC/friends/picpic.pic'/>">피크픽's Pick!</a></li>
-													 <li><a href="#">베스트 픽</a></li>
-													 <li><a href="#">피크픽 명소찾기</a></li>
-											 </ul>
-									 </li>
-									 
-									 
-									 
-									 <li><a href="#">LOGIN</a>
-							 <li><a href="#">SING UP</a>
-									 
-								 </ul>
-							</nav>
-							<!-- 모바일 용 서브 메뉴 창 -->
-							
-							
-							
-							
-					 </div>
-					 
-					 <div class="button-container-1">
-							 <span class="mas">Menu</span>
-							 <button id='work' type="button" name="Hover">Menu</button>
-						 </div>
-						 
-						 
-					 <!-- 모바일 용 메뉴 -->
-						<div class="nav_wrap_top_position">
-				
-						 <div class="nav_wrap_main">
-									 <div class="nav_wrap">
-											<ul class="ul_submenu">
-											 <li><a href="<c:url value='/PICKPIC/story/back.pic'/> "> 개발배경</a></li>
-													 <li><a href="<c:url value='/PICKPIC/story/culture.pic'/> ">피크픽 문화</a></li>
-												 <li><a href="#">팀원소개</a></li>
-												 <li><a href="#">앞으로의 행보</a></li>
-											</ul>
-									 </div>
-			 
-									 <div class="nav_wrap">
-											<ul class="ul_submenu" >
-												 <li><a href="#">필터링</a></li>
-												 <li><a href="<c:url value='/PickPic/friends/Place.bbs'/>">명소찾아주기</a></li>
-												 <li><a href="#">피크픽추천</a></li>
-											</ul>
-									 </div>
-			 
-									 <div class="nav_wrap">
-											<ul class="ul_submenu" >
-												 <li><a href="#">피크픽 이렇게 활용하세요</a></li>
-												 <li><a href="<c:url value='/PICKPIC/friends/picpic.pic'/> ">피크픽's Pick!</a></li>
-												 <li><a href="#">베스트 픽</a></li>
-												 <li><a href="#">피크픽 명소찾기</a></li>
-											</ul>
-									 </div>
-			 
-									 
-								</div>
-						</div> 
-				</div>
-				
-				<!-- Analytics 추적 코드 -->
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-138587279-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<header class="container-fluid header_top ">
 
-  gtag('config', 'UA-138587279-1');
-</script>
-			</header>
-				
+	<div class="menu_all_wrap_position">
+
+		<div class="logo">
+			<a href="<c:url value='/PICKPIC/admin/home.pic'/>">
+			<img id="top_logo" src="<c:url value='/resources/images/pickpic_logo_white.png'/>"/></a>
+		</div>
+
+		<div class="menu_login_wrap">
+			<div class="menu_login_in">
+				<nav class="menu_wrap">
+					<ul>
+						<li><a href="#">피크픽 스토리</a></li>
+						<li><a href="#">피크픽 프렌즈</a></li>
+						<li><a href="#">고객센터</a></li>
+					</ul>
+
+				</nav>
+
+
+				<div class="login_wrap">
+					<ul>
+						<li><a href="<c:url value='/PickPic/login/Login.pic'/>">LOGIN<span></span></a>
+						<li><a href="<c:url value='/PickPic/login/Sign_Up.pic'/>">SING UP</a>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- 모바일 용 메뉴 -->
+		<div id="mobile-nav">
+			<nav>
+				<ul class="menu">
+
+
+					<li class="l_menu_li"><a href="#">피크픽 스토리</a>
+						<ul class="m_sub">
+							<li><a href="<c:url value='/PICKPIC/story/back.pic'/> ">
+									개발배경</a></li>
+							<li><a href="<c:url value='/PICKPIC/story/culture.pic'/> ">피크픽
+									문화</a></li>
+							<li><a href="#">팀원소개</a></li>
+							<li><a href="#">앞으로의 행보</a></li>
+						</ul></li>
+
+
+					<li class="l_menu_li"><a href="#">피크픽 프렌즈</a>
+						<ul class="m_sub" style="display: none;">
+							<li><a href="#">필터링</a></li>
+							<li><a href="<c:url value='/PickPic/friends/Place.pic'/>">명소찾아주기</a></li>
+							<li><a href="#">피크픽추천</a></li>
+						</ul></li>
+
+
+					<li class="l_menu_li"><a href="#">고객센터</a>
+						<ul class="m_sub" style="display: none;">
+							<li><a href="#">피크픽 이렇게 활용하세요</a></li>
+							<li><a href="<c:url value='/PICKPIC/friends/picpic.pic'/>">피크픽's
+									Pick!</a></li>
+							<li><a href="#">베스트 픽</a></li>
+							<li><a href="#">피크픽 명소찾기</a></li>
+						</ul></li>
+
+
+
+					<li><a href="<c:url value='/PickPic/login/Login.pic'/>">LOGIN</a>
+					<li><a href="<c:url value='/PickPic/login/Sign_Up.pic'/>">SING UP</a>
+				</ul>
+			</nav>
+			<!-- 모바일 용 서브 메뉴 창 -->
+
+
+
+
+		</div>
+
+		<div class="button-container-1">
+			<span class="mas">Menu</span>
+			<button id='work' type="button" name="Hover">Menu</button>
+		</div>
+
+
+		<!-- 모바일 용 메뉴 -->
+		<div class="nav_wrap_top_position">
+
+			<div class="nav_wrap_main">
+				<div class="nav_wrap">
+					<ul class="ul_submenu">
+						<li><a href="<c:url value='/PICKPIC/story/back.pic'/> ">
+								개발배경</a></li>
+						<li><a href="<c:url value='/PICKPIC/story/culture.pic'/> ">피크픽
+								문화</a></li>
+						<li><a href="#">팀원소개</a></li>
+						<li><a href="#">앞으로의 행보</a></li>
+					</ul>
+				</div>
+
+				<div class="nav_wrap">
+					<ul class="ul_submenu">
+						<li><a href="#">필터링</a></li>
+						<li><a href="<c:url value='/PickPic/friends/Place.pic'/>">명소찾아주기</a></li>
+						<li><a href="#">피크픽추천</a></li>
+					</ul>
+				</div>
+
+				<div class="nav_wrap">
+					<ul class="ul_submenu">
+						<li><a href="#">피크픽 이렇게 활용하세요</a></li>
+						<li><a href="<c:url value='/PICKPIC/friends/picpic.pic'/> ">피크픽's
+								Pick!</a></li>
+						<li><a href="#">베스트 픽</a></li>
+						<li><a href="#">피크픽 명소찾기</a></li>
+					</ul>
+				</div>
+
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Analytics 추적 코드 -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async
+		src="https://www.googletagmanager.com/gtag/js?id=UA-138587279-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-138587279-1');
+	</script>
+</header>
+
 
 
 
--- PICKPIC/src/main/webapp/js/Top.js
+++ PICKPIC/src/main/webapp/js/Top.js
@@ -1,128 +1,136 @@
+$(function() {
 
-$(function(){
-	
-	
-	
-	var  l_masonry_wrap = document.getElementsByClassName("l_masonry_wrap");
+	var l_masonry_wrap = document.getElementsByClassName("l_masonry_wrap");
 	var grid = document.getElementsByClassName("grid");
 	var grid_item = document.getElementsByClassName("grid-item");
 	l_masonry_wrap.innerHTML = '';
-	
-	/*for(var i=0;i<17;i++){
-		grid_item[i].innerHTML = '<img src="img/'+i+1+'.jpg" >';
-	}*/
-	
-	
-	
-	/*grid[0].innerHTML = '<div class="grid-item" ><img src="img/1.jpg" ></div><div class="grid-item" ><img src="img/2.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/3.jpg" ></div><div class="grid-item" ><img src="img/4.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/5.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/6.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/7.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/8.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/10.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/11.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/12.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/13.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/14.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/15.jpg" ></div>'
-		+'<div class="grid-item" ><img src="img/16.jpg"></div>'
-		+'<div class="grid-item" ><img src="img/17.jpg"></div>';*/
-	
-		
-	
-	
-	/*	<div class="grid">
-	
 
-	<div class="grid-item" ><img src="img/2.jpg" ></div>
-	<div class="grid-item" ><img src="img/3.jpg" ></div>
-	<div class="grid-item" ><img src="img/4.jpg" ></div>
-	<div class="grid-item" ><img src="img/5.jpg" ></div>
-	<div class="grid-item" ><img src="img/6.jpg" ></div>
-	<div class="grid-item" ><img src="img/7.jpg" ></div>
-	<div class="grid-item" ><img src="img/8.jpg" ></div>
-	<div class="grid-item" ><img src="img/10.jpg" ></div>
-	<div class="grid-item" ><img src="img/11.jpg" ></div>
-	<div class="grid-item" ><img src="img/12.jpg" ></div>
-	<div class="grid-item" ><img src="img/13.jpg" ></div>
-	<div class="grid-item" ><img src="img/14.jpg" ></div>
-	<div class="grid-item" ><img src="img/15.jpg" ></div>
-	<div class="grid-item" ><img src="img/16.jpg"></div>
-	<div class="grid-item" ><img src="img/17.jpg"></div> 
-	</div>*/
+	/*
+	 * for(var i=0;i<17;i++){ grid_item[i].innerHTML = '<img
+	 * src="img/'+i+1+'.jpg" >'; }
+	 */
+
+	/*
+	 * grid[0].innerHTML = '<div class="grid-item" ><img src="img/1.jpg" ></div><div
+	 * class="grid-item" ><img src="img/2.jpg" ></div>' +'<div
+	 * class="grid-item" ><img src="img/3.jpg" ></div><div class="grid-item" ><img
+	 * src="img/4.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/5.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/6.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/7.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/8.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/10.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/11.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/12.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/13.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/14.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/15.jpg" ></div>' +'<div class="grid-item" ><img
+	 * src="img/16.jpg"></div>' +'<div class="grid-item" ><img
+	 * src="img/17.jpg"></div>';
+	 */
+
+	/*
+	 * <div class="grid">
+	 * 
+	 * 
+	 * <div class="grid-item" ><img src="img/2.jpg" ></div> <div
+	 * class="grid-item" ><img src="img/3.jpg" ></div> <div class="grid-item" ><img
+	 * src="img/4.jpg" ></div> <div class="grid-item" ><img src="img/5.jpg" ></div>
+	 * <div class="grid-item" ><img src="img/6.jpg" ></div> <div
+	 * class="grid-item" ><img src="img/7.jpg" ></div> <div class="grid-item" ><img
+	 * src="img/8.jpg" ></div> <div class="grid-item" ><img src="img/10.jpg" ></div>
+	 * <div class="grid-item" ><img src="img/11.jpg" ></div> <div
+	 * class="grid-item" ><img src="img/12.jpg" ></div> <div class="grid-item" ><img
+	 * src="img/13.jpg" ></div> <div class="grid-item" ><img src="img/14.jpg" ></div>
+	 * <div class="grid-item" ><img src="img/15.jpg" ></div> <div
+	 * class="grid-item" ><img src="img/16.jpg"></div> <div class="grid-item" ><img
+	 * src="img/17.jpg"></div> </div>
+	 */
+
+	/*
+	 * for(var i=0;i<99999;i++){ console.log(i); }
+	 */
+
+	$(".menu_wrap ul li").mouseenter(function() {
+		$('.menu_all_wrap_position').css({
+			height : "250px"
+		});
+		$(".nav_wrap_top_position").stop().animate().slideDown();
+	});
+	$(".nav_wrap_top_position").mouseleave(function() {
+
+		$(this).stop().animate().slideUp(function() {
+			$('.menu_all_wrap_position').css({
+				height : "50px"
+			});
+		});
+
+	});
+
+	var fn_addFunction = function() {
+		$(".pic_gif").addClass('pic_gifadd');
+		$(".header_top").addClass('scroll');
+		$(".nav_wrap_top_position").addClass('scroll');
+		$(".menu_wrap a").addClass('scroll_color');
+		$(".ul_submenu li a").addClass('scroll_color2');
+		$(".login_wrap ul li a").addClass('scroll_login');
+		$(".login_wrap ul li a span").addClass('scroll_span');
+//		$("#top_logo").
+	};
+	var fn_removeFunction = function() {
+		$(".pic_gif").removeClass('pic_gifadd');
+		$(".header_top").removeClass('scroll');
+		$(".nav_wrap_top_position").removeClass('scroll');
+		$(".menu_wrap a").removeClass('scroll_color');
+		$(".ul_submenu li a").removeClass('scroll_color2');
+		$(".login_wrap ul li a").removeClass('scroll_login');
+		$(".login_wrap ul li a span").removeClass('scroll_span');
+	};
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > "0") {
+			fn_addFunction();
+		} else {
+			fn_removeFunction();
+		}
+	});
 	
+	$('.header_top').mouseenter(function() {
+		fn_addFunction();
+	});
 	
-/*	for(var i=0;i<99999;i++){
-		console.log(i);
-	}*/
-	
-	
-	
-	
-	  $(".menu_wrap ul li").mouseenter(function(){
-		  $('.menu_all_wrap_position').css({height:"250px"});
-	         $(".nav_wrap_top_position").stop().animate().slideDown();      
-	      });
-	       $(".nav_wrap_top_position").mouseleave(function(){
-	    	  
-	         $(this).stop().animate().slideUp(function(){
-	        	 $('.menu_all_wrap_position').css({height:"50px"});
-	         }); 
-	         
-	      });  
-	       
-	       
-	      $(window).scroll(function(){
-	   		if($(this).scrollTop() > "0"){
-	   			
-	   			$(".pic_gif").addClass('pic_gifadd');
-	   			$(".header_top").addClass('scroll');
-	   			$(".nav_wrap_top_position").addClass('scroll');
-	   			$(".menu_wrap a").addClass('scroll_color');
-	   			$(".ul_submenu li a").addClass('scroll_color2');
-	   			$(".login_wrap ul li a").addClass('scroll_login');
-	   			$(".login_wrap ul li a span").addClass('scroll_span');
-	   		}else{
-	   			$(".pic_gif").removeClass('pic_gifadd');
-	   			$(".header_top").removeClass('scroll');
-	   			$(".nav_wrap_top_position").removeClass('scroll');
-	   			$(".menu_wrap a").removeClass('scroll_color');
-	   			$(".ul_submenu li a").removeClass('scroll_color2');
-	   			$(".login_wrap ul li a").removeClass('scroll_login');
-	   			$(".login_wrap ul li a span").removeClass('scroll_span');
-	   		} 	
-   		});
-	     
-	      
-	      
-	      
-	    $('.button-container-1').click(function(e){
-	    	if($('#mobile-nav').css("left") == "0px"){
-	    		
-	    		$('#mobile-nav').stop().animate({left:"-500px"},500,function(){
-	    			$('.menu_all_wrap_position').css({height:"50px"});
-	    		});
-	    	}else{
-	    		$('.menu_all_wrap_position').css({height:"940px"});
-	    		$('#mobile-nav').stop().animate({left:"0px"},500);
-	    	}
-	    });
-	    	
-	    
-	    
-		
-		 $(".l_menu_li").mouseenter(function(){
-	         $(this).find('.m_sub').stop().animate().slideDown();
-	         $(".l_menu_li").not(this).find(".m_sub").stop().animate().slideUp();
-	         
-	         
-	      });
-	       $(".menu").mouseleave(function(){
-	    	   //$(this).find('.m_sub').stop().animate().slideUp();
-	    	   
-	    	  
-	      }); 
-	       
-	       
-   });
+	$('.header_top').mouseleave(function() {
+		fn_removeFunction();
+	});
+
+	$('.button-container-1').click(function(e) {
+		if ($('#mobile-nav').css("left") == "0px") {
+
+			$('#mobile-nav').stop().animate({
+				left : "-500px"
+			}, 500, function() {
+				$('.menu_all_wrap_position').css({
+					height : "50px"
+				});
+			});
+		} else {
+			$('.menu_all_wrap_position').css({
+				height : "940px"
+			});
+			$('#mobile-nav').stop().animate({
+				left : "0px"
+			}, 500);
+		}
+	});
+
+	$(".l_menu_li").mouseenter(function() {
+		$(this).find('.m_sub').stop().animate().slideDown();
+		$(".l_menu_li").not(this).find(".m_sub").stop().animate().slideUp();
+
+	});
+	$(".menu").mouseleave(function() {
+		// $(this).find('.m_sub').stop().animate().slideUp();
+
+	});
+
+});
