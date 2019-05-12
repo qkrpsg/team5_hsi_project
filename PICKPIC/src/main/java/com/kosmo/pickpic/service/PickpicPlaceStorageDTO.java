9039360a29158ba7f09e443fa681579c258ac681
psg <skpar16@naver.com>
2019-05-12 15:01:22 +0900

    dto수정

--- PICKPIC/src/main/java/com/kosmo/pickpic/service/PickpicPlaceStorageDTO.java
+++ PICKPIC/src/main/java/com/kosmo/pickpic/service/PickpicPlaceStorageDTO.java
@@ -2,27 +2,33 @@ package com.kosmo.pickpic.service;
 
 public class PickpicPlaceStorageDTO {
 	//객체들]
-	private String placeStorage_no;
-	private String place_no;
-	private String id;
+	private String pps_id;
+	private String pps_index;
+	private String ppb_id;
+	private String ppa_id;
 	//게터/세터]
-	public String getPlaceStorage_no() {
-		return placeStorage_no;
-	}//getPlaceStorage_no
-	public void setPlaceStorage_no(String placeStorage_no) {
-		this.placeStorage_no = placeStorage_no;
-	}//setPlaceStorage_no
-	public String getPlace_no() {
-		return place_no;
-	}//getPlace_no
-	public void setPlace_no(String place_no) {
-		this.place_no = place_no;
-	}//setPlace_no
-	public String getId() {
-		return id;
-	}//getId
-	public void setId(String id) {
-		this.id = id;
-	}//setId
-	
+	public String getPps_id() {
+		return pps_id;
+	}//getPps_id
+	public void setPps_id(String pps_id) {
+		this.pps_id = pps_id;
+	}//setPps_id
+	public String getPps_index() {
+		return pps_index;
+	}//getPps_index
+	public void setPps_index(String pps_index) {
+		this.pps_index = pps_index;
+	}//setPps_index
+	public String getPpb_id() {
+		return ppb_id;
+	}//getPpb_id
+	public void setPpb_id(String ppb_id) {
+		this.ppb_id = ppb_id;
+	}//setPpb_id
+	public String getPpa_id() {
+		return ppa_id;
+	}//getPpa_id
+	public void setPpa_id(String ppa_id) {
+		this.ppa_id = ppa_id;
+	}//setPpa_id
 }//PickpicPlaceStorageDTO
