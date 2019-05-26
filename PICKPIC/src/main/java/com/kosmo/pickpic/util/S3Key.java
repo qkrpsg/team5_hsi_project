8de5c213e9036cd4efea80bf1c6091fb0d5eb8b7
최명범 <audqja3400@naver.com>
2019-05-26 17:33:13 +0900

    그만

--- PICKPIC/src/main/java/com/kosmo/pickpic/util/S3Key.java
+++ PICKPIC/src/main/java/com/kosmo/pickpic/util/S3Key.java
@@ -1,6 +1,6 @@
 package com.kosmo.pickpic.util;
 
 public class S3Key {
-	public static String accessKey = "두"; // 엑세스 키
-	public static String secretKey = "식"; // 보안 엑세스 키
+	public static String accessKey = "그만좀"; // 엑세스 키
+	public static String secretKey = "바껴라"; // 보안 엑세스 키
 }
